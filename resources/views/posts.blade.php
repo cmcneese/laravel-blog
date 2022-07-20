3e9d71db94826a277cbb9d1a8d8dca52c200579e
Chris McNeese <christophermcneese@mac.com>
2022-07-20 10:11:09 -0500

    save point for first eloquent model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,8 +7,11 @@
                 </a>
             </h1>
 
+            <p>
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
             <div>
-                {{ $post->excerpt }}
+                {!! $post->excerpt !!}
             </div>
         </article>
     @endforeach
